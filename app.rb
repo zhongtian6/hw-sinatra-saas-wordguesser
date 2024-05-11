a6cd7a8c975fe3b938999500355e9b8bab5ef623
zhong <ztian6@ncsu.edu>
2024-05-11 14:58:24 -0400

    finish app

--- app.rb
+++ app.rb
@@ -6,25 +6,26 @@ class WordGuesserApp < Sinatra::Base
 
   enable :sessions
   register Sinatra::Flash
-  
+
   before do
     @game = session[:game] || WordGuesserGame.new('')
   end
-  
+
   after do
     session[:game] = @game
   end
-  
+
   # These two routes are good examples of Sinatra syntax
   # to help you with the rest of the assignment
   get '/' do
     redirect '/new'
   end
-  
+
   get '/new' do
+    session[:game_status] = :play
     erb :new
   end
-  
+
   post '/create' do
     # NOTE: don't change next line - it's needed by autograder!
     word = params[:word] || WordGuesserGame.get_random_word
@@ -33,16 +34,25 @@ class WordGuesserApp < Sinatra::Base
     @game = WordGuesserGame.new(word)
     redirect '/show'
   end
-  
+
   # Use existing methods in WordGuesserGame to process a guess.
   # If a guess is repeated, set flash[:message] to "You have already used that letter."
   # If a guess is invalid, set flash[:message] to "Invalid guess."
   post '/guess' do
     letter = params[:guess].to_s[0]
     ### YOUR CODE HERE ###
+    begin
+      guess_status = @game.guess(letter)
+    rescue ArgumentError => e
+      flash[:message] = "Invalid guess."
+    else
+      if !guess_status
+        flash[:message] = "You have already used that letter."
+      end
+    end
     redirect '/show'
   end
-  
+
   # Everytime a guess is made, we should eventually end up at this route.
   # Use existing methods in WordGuesserGame to check if player has
   # won, lost, or neither, and take the appropriate action.
@@ -50,17 +60,35 @@ class WordGuesserApp < Sinatra::Base
   # wrong_guesses and word_with_guesses from @game.
   get '/show' do
     ### YOUR CODE HERE ###
-    erb :show # You may change/remove this line
+    #erb :show # You may change/remove this line
+    session[:game_status] = @game.check_win_or_lose
+    if session[:game_status] == :win
+      redirect '/win'
+    elsif session[:game_status] == :lose
+      redirect 'lose'
+    else
+      erb :show
+    end
   end
-  
+
   get '/win' do
     ### YOUR CODE HERE ###
-    erb :win # You may change/remove this line
+    # erb :win # You may change/remove this line
+    if session[:game_status] == :play
+      redirect '/show'
+    else
+      erb :win
+    end
   end
-  
+
   get '/lose' do
     ### YOUR CODE HERE ###
-    erb :lose # You may change/remove this line
+    # erb :lose # You may change/remove this line
+    if session[:game_status] == :play
+      redirect '/show'
+    else
+      erb :lose
+    end
   end
-  
+
 end
